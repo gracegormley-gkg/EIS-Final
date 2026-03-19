2f4fed6f7e146a4b569f2fcead08307fe9c6b2d4
Grace Gormley <gracegormley@dhcp-10-105-179-38.wireless.northwestern.private>
2026-03-19 15:04:35 -0500

    Revert "Add network visualization, document map, and EIS map components"
    This reverts commit 53363dcba74247ed581637408bc6f619fa68d29e.

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -18,7 +18,6 @@ export const clientComponents = {
   StoryMapJS: './StoryMapJS.client.tsx',
   ExhibitionModules: './ExhibitionModules.tsx',
   SigmaExample: './SigmaExample.Client.jsx',
-  EISMap: './EISMap.client.jsx',
   EISAnnotations: './EISAnnotations.client.tsx',
   EISFilterBar: './EISFilterBar.client.tsx',
   EISRelatedWorks: './EISRelatedWorks.client.tsx',
